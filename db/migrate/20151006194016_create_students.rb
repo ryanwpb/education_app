9aa0fe125b33c50bcb2d74f27710aaa3971d0c6d
Ryan Graham <ryangrahamwpb@gmail.com>
2015-10-06 16:41:16 -0400

    first commit

---
+++ db/migrate/20151006194016_create_students.rb
@@ -0,0 +1,14 @@
+class CreateStudents < ActiveRecord::Migration
+  def change
+    create_table :students do |t|
+      t.string :name
+      t.string :email
+      t.string :password_digest
+      t.integer :teacher_id
+      t.integer :parent_id
+    
+
+      t.timestamps null: false
+    end
+  end
+end
